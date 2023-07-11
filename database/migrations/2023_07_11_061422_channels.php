c29100b4d1cebeec7980b18b9a454e1aec4e1370
Amirhossein Zareian <amirhosseinzareian01@gmail.com>
2023-07-11 10:16:49 +0330

    add channels & posts table & edit users table structure

---
+++ database/migrations/2023_07_11_061422_channels.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('channels', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('description');
+            $table->string('channel_telegram_id');
+            $table->integer('members_count');
+            $table->integer('view');
+            $table->integer('share');
+            $table->string('tags');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
