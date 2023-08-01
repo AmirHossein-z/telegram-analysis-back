78a5b5921a1e2e722cb5ae051727288646fb7575
Amirhossein Zareian <amirhosseinzareian01@gmail.com>
2023-08-01 18:40:14 +0330

    add filter feature

--- app/Http/Controllers/ChannelController.php
+++ app/Http/Controllers/ChannelController.php
@@ -8,24 +8,89 @@ use danog\MadelineProto\API;
 
 class ChannelController extends Controller
 {
+    // public function getByUserId($userId, Request $request, $filter = null)
+    // {
+    //     try {
+    //         $page = $request->query('page', 1);
+    //         $limit = 10;
+
+    //         $query = DB::table('channels')->where('user_id', $userId);
+
+    //         if ($filter === 'bestView') {
+    //             $query->orderByDesc('view');
+    //         } else if ($filter === 'leastView') {
+    //             $query->orderBy('view');
+    //         } else if ($filter === 'bestShare') {
+    //             $query->orderByDesc('share');
+    //         } else if ($filter === 'leastShare') {
+    //             $query->orderBy('share');
+    //         }
+
+    //         $channels = $query->paginate($limit, ['*'], 'page', $page);
+
+    //         if ($channels->count() > 0) {
+    //             return response()->json(['status' => true, 'value' => $channels]);
+    //         } else {
+    //             return response()->json(['status' => false, 'value' => []]);
+    //         }
+    //     } catch (\Throwable $e) {
+    //         return response()->json($e);
+    //     }
+    // }
+    // public function getByUserId($userId, Request $request, $filter = null)
+    // {
+    //     try {
+    //         $page = $request->query('page', 1);
+    //         $limit = 10;
+    //         $offset = ($page - 1) * $limit;
+
+    //         $query = DB::table('channels')->where('user_id', $userId);
+
+    //         if ($filter === 'bestView') {
+    //             $query->orderByDesc('view');
+    //         } else if ($filter === 'leastView') {
+    //             $query->orderBy('view');
+    //         } else if ($filter === 'bestShare') {
+    //             $query->orderByDesc('share');
+    //         } else if ($filter === 'leastShare') {
+    //             $query->orderBy('share');
+    //         }
+
+    //         $channels = $query->offset($offset)->limit($limit)->get();
+
+    //         if (count($channels) > 0) {
+    //             return response()->json(['status' => true, 'value' => $channels]);
+    //         } else {
+    //             return response()->json(['status' => false, 'value' => []]);
+    //         }
+    //     } catch (\Throwable $e) {
+    //         return response()->json($e);
+    //     }
+    // }
+
     public function getByUserId($userId, Request $request)
     {
-        // $channels = DB::select('SELECT * FROM channels WHERE user_id = ?', [$userId]);
-
-        // if (count($channels) > 0) {
-        //     return response()->json(['status' => true, 'value' => $channels]);
-
-        // } else {
-        //     return response()->json(['status' => false, 'value' => []]);
-        // }
         try {
             $page = $request->query('page', 1);
+            $filter = $request->query('filter', null);
+            $limit = 10;
 
-            $channels = DB::table('channels')->where('user_id', $userId)->paginate(10);
+            $query = DB::table('channels')->where('user_id', $userId);
 
-            if (count($channels) > 0) {
-                return response()->json(['status' => true, 'value' => $channels]);
+            if ($filter === 'bestView') {
+                $query->orderByDesc('view');
+            } else if ($filter === 'leastView') {
+                $query->orderBy('view');
+            } else if ($filter === 'bestShare') {
+                $query->orderByDesc('share');
+            } else if ($filter === 'leastShare') {
+                $query->orderBy('share');
+            }
 
+            $channels = $query->paginate($limit, ['*'], 'page', $page);
+
+            if ($channels->count() > 0) {
+                return response()->json(['status' => true, 'value' => $channels, 'filter' => $filter]);
             } else {
                 return response()->json(['status' => false, 'value' => []]);
             }
@@ -33,6 +98,23 @@ class ChannelController extends Controller
             return response()->json($e);
         }
     }
+    // public function getByUserId($userId, Request $request)
+    // {
+    //     try {
+    //         $page = $request->query('page', 1);
+
+    //         $channels = DB::table('channels')->where('user_id', $userId)->paginate(10);
+
+    //         if (count($channels) > 0) {
+    //             return response()->json(['status' => true, 'value' => $channels]);
+
+    //         } else {
+    //             return response()->json(['status' => false, 'value' => []]);
+    //         }
+    //     } catch (\Throwable $e) {
+    //         return response()->json($e);
+    //     }
+    // }
 
     public function getChannel($channelId)
     {
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class PostController extends Controller
 {
@@ -18,19 +19,31 @@ class PostController extends Controller
     {
         try {
             $page = $request->query('page', 1);
+            $filter = $request->query('filter', null);
+            $limit = 10;
 
-            $posts = DB::table('posts')->where('channel_id', $channelId)->paginate(10);
+            $query = DB::table('posts')->where('channel_id', $channelId);
 
-            if (count($posts) > 0) {
+            if ($filter === 'bestView') {
+                $query->orderByDesc('view');
+            } else if ($filter === 'leastView') {
+                $query->orderBy('view');
+            } else if ($filter === 'bestShare') {
+                $query->orderByDesc('share');
+            } else if ($filter === 'leastShare') {
+                $query->orderBy('share');
+            }
+
+            $posts = $query->paginate($limit, ['*'], 'page', $page);
+
+            if ($posts->count() > 0) {
                 return response()->json(['status' => true, 'value' => $posts]);
-
             } else {
                 return response()->json(['status' => false, 'value' => []]);
             }
         } catch (\Throwable $e) {
             return response()->json($e);
         }
-
     }
 
     public function getPost($postId)
@@ -60,10 +73,19 @@ class PostController extends Controller
     public function getPostsStat($channelId)
     {
         try {
-            $views = DB::select('SELECT id, view, created_at FROM posts WHERE channel_id = ? ORDER BY created_at ASC', [$channelId]);
-            $shares = DB::select('SELECT id, share, created_at FROM posts WHERE channel_id = ? ORDER BY created_at ASC', [$channelId]);
+            $posts = DB::select('SELECT id, view, share, created_at FROM posts WHERE channel_id = ? ORDER BY created_at ASC', [$channelId]);
 
-            return response()->json(['status' => true, 'value' => ['view' => $views, 'share' => $shares]]);
+            $views = [];
+            $shares = [];
+            $dates = [];
+
+            foreach ($posts as $post) {
+                $views[] = ['id' => $post->id, 'view' => $post->view, 'created_at' => $post->created_at];
+                $shares[] = ['id' => $post->id, 'share' => $post->share, 'created_at' => $post->created_at];
+                $dates[] = ['id' => $post->id, 'created_at' => $post->created_at];
+            }
+
+            return response()->json(['status' => true, 'value' => ['view' => $views, 'share' => $shares, 'date' => $dates]]);
         } catch (\Throwable $e) {
             return response()->json(['status' => false, 'value' => $e]);
         }
